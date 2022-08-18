35a4886ad95e74820f0a55e2facef56aad10f63f
Onur Yuruten <onuryuruten@gmail.com>
2022-08-18 15:49:29 +0200

    remove undesired inclusions

--- s3prl/hub.py
+++ s3prl/hub.py
@@ -11,8 +11,8 @@ from s3prl.upstream.distiller.hubconf import *
 from s3prl.upstream.example.hubconf import *
 from s3prl.upstream.hubert.hubconf import *
 # from s3prl.upstream.lighthubert.hubconf import *  # FIXME: (Leo)
-from s3prl.upstream.log_stft.hubconf import *
-from s3prl.upstream.mockingjay.hubconf import *
+#from s3prl.upstream.log_stft.hubconf import *
+#from s3prl.upstream.mockingjay.hubconf import *
 from s3prl.upstream.mos_prediction.hubconf import *
 from s3prl.upstream.npc.hubconf import *
 from s3prl.upstream.pase.hubconf import *
